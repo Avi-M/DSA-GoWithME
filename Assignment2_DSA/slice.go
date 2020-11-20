ec0847e0bc598c036b520fec4fd664025228e960
Avinash Maurya <53138649+Avi-M@users.noreply.github.com>
2020-11-20 13:48:32 +0530

    Update slice.go

--- Assignment2_DSA/slice.go
+++ Assignment2_DSA/slice.go
@@ -14,10 +14,18 @@ func main() {
 	slice2 := make([]int, 10) // define with capacity
 	fmt.Println(cap(slice2))
 	// define 2 dim slice
-	rows := 8
-	cols := 8
-	slice3 := make([][]int, rows)
-	for i := range slice3 {
-		slice3[i] = make([]int, cols)
+	twoDSlice1 := make([][]int, 2)
+	twoDSlice1[0] = []int{1, 2, 3}
+	twoDSlice1[1] = []int{4, 5, 6}
+	fmt.Println()
+	fmt.Printf("Number of rows in slice: %d\n", len(twoDSlice1))
+	fmt.Printf("Number of columns in arsliceray: %d\n", len(twoDSlice1[0]))
+	fmt.Printf("Total number of elements in slice: %d\n", len(twoDSlice1)*len(twoDSlice1[0]))
+	fmt.Println("First Slice")
+	for _, row := range twoDSlice1 {
+		for _, val := range row {
+			fmt.Print(" ", val)
+		}
+		fmt.Println()
 	}
 }
